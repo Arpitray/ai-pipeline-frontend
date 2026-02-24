119c49d506d8d5e35aa57edea8811fdb1549215d
Arpit <rayarpit72@gmail.com>
2026-02-25 03:34:18 +0530

    again again 18

--- app/page.jsx
+++ app/page.jsx
@@ -131,8 +131,8 @@ export default async function HomePage() {
 
         {products.length > 0 ? (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-24">
-            {products.map((product) => (
-              <ProductCard key={product.id} product={product} />
+            {products.map((product, i) => (
+              <ProductCard key={product.id} product={product} index={i} />
             ))}
           </div>
         ) : (
